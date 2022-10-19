a3510171254d9a6e7a3d19ad189e6a77e56da59d
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-10-19 11:41:01 +0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { RotatedBox } from "./components/rotatedBox/rotatedBox";
+import { Box } from "@mui/material";
+import { AdditionalInfo } from "./components/additionalInfo/additionalInfo";
+import { InfoContainer, InfoWrapper, MainBlock } from "./styled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainBlock>
+      <RotatedBox />
+      <InfoContainer>
+        <InfoWrapper>
+          <AdditionalInfo />
+        </InfoWrapper>
+        <Box flex={3} />
+      </InfoContainer>
+    </MainBlock>
   );
 }
 
